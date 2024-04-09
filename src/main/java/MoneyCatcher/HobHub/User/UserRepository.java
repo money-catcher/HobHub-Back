4fcf03cd0be145341bb55c4e6269a582f00d2815
jin171 <yujin10347@ewhian.net>
2024-04-09 16:51:31 +0900

    return

--- src/main/java/MoneyCatcher/HobHub/User/UserRepository.java
+++ src/main/java/MoneyCatcher/HobHub/User/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<UserEntity> findByAge(int age);
     List<UserEntity> findByHome(String home);
-    HobbyEntity findByHobby(String hobby);
+
 }

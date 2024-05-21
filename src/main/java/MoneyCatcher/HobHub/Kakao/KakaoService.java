8dd8e0420510e0d57d3a539beee003f33306ab7b
Haejin Park <104544503+hyp00@users.noreply.github.com>
2024-05-21 18:12:19 +0900

    Update KakaoService.java

--- src/main/java/MoneyCatcher/HobHub/Kakao/KakaoService.java
+++ src/main/java/MoneyCatcher/HobHub/Kakao/KakaoService.java
@@ -3,9 +3,10 @@ package MoneyCatcher.HobHub.Kakao;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -15,19 +16,26 @@ import reactor.core.publisher.Mono;
 @Service
 public class KakaoService {
 
-    @Value("${kakao.client_id}")
     private String client_id;
+    private final String KATUH_TOKEN_URL;
+    private final String KAUTH_USER_URL;
 
-    private final WebClient webClient;
+    @Autowired
+    public KakaoService(@Value("${kakao.client_id}") String client_id) {
+        this.client_id = client_id;
+        this.KATUH_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+        this.KAUTH_USER_URL = "https://kapi.kakao.com/v2/user/me";
+    }
 
     public String getAccessTokenFromKakao(String code) {
 
-        String urlString = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&" +
-                "client_id="+ client_id+
-                "&code=" + code;
-
         KakaoTokenResponseDto kakaoTokenResponseDto = WebClient.create().post()
-                .uri(urlString)
+                .uri(uriBuilder -> uriBuilder
+                        .path(KATUH_TOKEN_URL)
+                        .queryParam("grant_type", "authorization_code")
+                        .queryParam("client_id", client_id)
+                        .queryParam("code", code)
+                        .build())
                 .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
                 .retrieve()
                 //TODO : Custom Exception
@@ -39,18 +47,35 @@ public class KakaoService {
 
         log.info(" [Kakao Service] Access Token ------> {}", kakaoTokenResponseDto.getAccessToken());
         log.info(" [Kakao Service] Refresh Token ------> {}", kakaoTokenResponseDto.getRefreshToken());
+        //제공 조건: OpenID Connect가 활성화 된 앱의 토큰 발급 요청인 경우 또는 scope에 openid를 포함한 추가 항목 동의 받기 요청을 거친 토큰 발급 요청인 경우
         log.info(" [Kakao Service] Id Token ------> {}", kakaoTokenResponseDto.getIdToken());
         log.info(" [Kakao Service] Scope ------> {}", kakaoTokenResponseDto.getScope());
 
         return kakaoTokenResponseDto.getAccessToken();
     }
 
+
+
+
     public KakaoUserInfoResponseDto getUserInfo(String accessToken) {
-        return this.webClient.get()
-                .uri("https://kapi.kakao.com/v2/user/me")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+
+        KakaoUserInfoResponseDto userInfo = WebClient.create()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(KAUTH_USER_URL)
+                        .build())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken) // access token 인가
+                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
                 .retrieve()
+                //TODO : Custom Exception
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.error(new RuntimeException("Invalid Parameter")))
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new RuntimeException("Internal Server Error")))
                 .bodyToMono(KakaoUserInfoResponseDto.class)
                 .block();
+
+        log.info("[ Kakao Service ] Auth ID ---> {} ", userInfo.getId());
+        log.info("[ Kakao Service ] NickName ---> {} ", userInfo.getKakaoAccount().getProfile().getNickName());
+
+        return userInfo;
     }
 }

e787db567def9dbc16d481b742853529d6b52182
Haejin Park <104544503+hyp00@users.noreply.github.com>
2024-05-21 18:09:30 +0900

    Add files via upload

---
+++ src/main/java/MoneyCatcher/HobHub/Kakao/KakaoLoginDto.java
@@ -0,0 +1,14 @@
+package KakaoLogin.demo.Kakao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}

22919f8c4416068000e9bb4d62323c2fb565a877
Haejin Park <104544503+hyp00@users.noreply.github.com>
2024-05-21 20:25:14 +0900

    Update KakaoLoginController.java

--- src/main/java/MoneyCatcher/HobHub/Kakao/KakaoLoginController.java
+++ src/main/java/MoneyCatcher/HobHub/Kakao/KakaoLoginController.java
@@ -2,14 +2,14 @@ package MoneyCatcher.HobHub.Kakao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -19,13 +19,22 @@ public class KakaoLoginController {
 
     private final KakaoService kakaoService;
 
-    @GetMapping("/callback")
-    public ResponseEntity<?> callback(@RequestParam("code") String code) {
+    @PostMapping("/callback")
+    public ResponseEntity<?> callback(@RequestBody Map<String, String> body) {
+        String code = body.get("code");
+        log.info("Received authorization code: {}", code);
+
+        if (code == null || code.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Authorization code is missing");
+        }
+
         String accessToken = kakaoService.getAccessTokenFromKakao(code);
+        log.info("Received access token: {}", accessToken);
 
         KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
+        log.info("Received user info: {}", userInfo);
 
-        // User 로그인, 또는 회원가입 로직 추가
-        return new ResponseEntity<>(HttpStatus.OK);
+        //로그인, 회원가입 로직 필요하면 추가
+        return ResponseEntity.ok(userInfo);
     }
 }
